0a4ba0e030678efe378b91e77549a64f79c39518
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-08-31 15:55:53 +0530

    Add files via upload

---
+++ type conversion.py
@@ -0,0 +1,41 @@
+#FLOAT TO INTEGER 
+
+a=10.5
+b=int(a)
+print(b)
+print(type(b))                  
+
+#INTEGER TO STRING
+
+a=10
+b=str(a)
+print(b)
+print(type(b))
+
+#FLOAT TO INTEGER
+
+a=10.5
+b=int(a)
+print(b)
+print(type(b))  
+
+#FLOAT TO STRING
+
+a=10.5
+b=str(a)
+print(b)
+print(type(b))
+
+#STRING TO INTEGER
+
+a="10"
+b=int(a)
+print(b)
+print(type(b))
+
+#STRING TO FLOAT
+
+a="10.5"
+b=float(a)
+print(b)
+print(type(b))
