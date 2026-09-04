b4979552993c7e744daa0206a0b61d8d81e21042
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-04 16:03:34 +0530

    Add files via upload

---
+++ WRITE A PYTHON PROGRAM TO SWAP TWO NUMBERS WITHOUT USING A THIRD VAIRABBLE.py
@@ -0,0 +1,11 @@
+  #python programing 5th experiement::5::
+
+     # WRITE A PYTHON PROGRAM TO SWAP TWO NUMBERS WITHOUT USING A THIRD VAIRABBLE:
+ 
+a=int(input("Enter a number a: "))
+b=int(input("Enter a number b: "))
+a=a+b
+b=a-b   
+a=a-b
+print("After swapping the values : a =", a)
+print("After swapping the values : b =", b)
