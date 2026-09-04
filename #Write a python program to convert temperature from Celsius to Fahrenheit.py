779dc966424a3fa6abc04ca5b43132125318c231
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-04 16:01:42 +0530

    Add files via upload

---
+++ #Write a python program to convert temperature from Celsius to Fahrenheit.py
@@ -0,0 +1,7 @@
+  #python programing 4th experiement::4::
+
+     #Write a python program to convert temperature from Celsius to Fahrenheit
+
+"""celsius=float(input("Enter temperature in Celsius: "))
+fahrenheit=(celsius * 9/5)+32
+print("Temperature in Fahrenheit:", fahrenheit)"""
