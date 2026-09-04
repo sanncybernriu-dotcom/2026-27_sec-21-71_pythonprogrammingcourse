d8e47cbfbaae811bc66b156c132fae634a8bff15
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-04 14:21:39 +0530

    Add files via upload

---
+++ # Identity Operators Exercise.py
@@ -0,0 +1,10 @@
+# Identity Operators Exercise
+a=[1,2,3]
+b=a
+result= a is b
+print("result of",b,"is",a,"is",result)
+
+c=[1,2,3]
+d=[1,2,3]
+result= d is c
+print("result of",d,"is",c,"is",result) 
