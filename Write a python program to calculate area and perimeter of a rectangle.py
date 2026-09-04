d3d5fcb2e51685d21e80c13eb946969f9da408d5
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-04 15:58:07 +0530

    Add files via upload

---
+++ Write a python program to calculate area and perimeter of a rectangle.py
@@ -0,0 +1,10 @@
+#python programing 3rd experiement::3::
+
+  # Write a python program to calculate area and perimeter of a rectangle
+
+"""length=-9
+breadth=5
+area=length*breadth
+print("area:", area)
+perimeter=2*(length+breadth)
+print("perimeter:", perimeter)"""
