088f5ce01fabc4ec28b2f2ea257b0659d99e0bd7
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-15 13:38:11 +0530

    Add files via upload

---
+++ experiment 1 - write a python program and display their sum.py
@@ -0,0 +1,8 @@
+#python programing 1st experiement::1::
+
+# Write a python program to read two numbers and display their sum
+
+"""num1=int(input("Enter num1"))
+num2=int(input("Enter num2"))
+result=num1+num2
+print(result)"""
