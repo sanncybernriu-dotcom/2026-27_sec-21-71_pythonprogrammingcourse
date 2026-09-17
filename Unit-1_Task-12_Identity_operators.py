61002922bbc4c0c52a4757eda09d4125239225ed
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-17 23:52:27 +0530

    Add files via upload

---
+++ Unit-1_Task-12_Identity_operators.py
@@ -0,0 +1,20 @@
+#IS OPERATOR
+a=[1,2,3,4]
+b=a
+result= a is b
+print("result of ",a, "is ",b, "is :",result)
+
+a=[1,2,3,4]
+b=[1,2,3,4]
+result= a is b
+print("result of ",a, "is",b, "is :",result)
+# IS NOT OPERATOR
+a=[1,2,3,4]
+b=a 
+result=a is not b
+print("result of ",a, "is not",b, "is :",result)
+
+a=[1,2,3,4]
+b=[1,2,3,4]
+result = a is not b
+print("result of ",a, "is not",b, "is :",result)
