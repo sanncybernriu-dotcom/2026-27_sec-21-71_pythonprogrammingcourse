34922372e981148b13283864206cf40e5798fb1a
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-17 23:50:25 +0530

    Add files via upload

---
+++ Unit-1_Task-10_Relational_Operators.py
@@ -0,0 +1,20 @@
+# Python Comparision Operators Exercise
+a=10
+b=5
+result = a > b # Greater than comparision
+print("result of",a, ">",b,"is : ",result)
+
+result = a < b # Less than comaparision
+print("result of",a,"<",b,"is :",result)
+
+result = a>=b # GREATER THAN EQUAL TO COMPARISION
+print("result of",a,">=",b,"is :",result)
+
+result = a<=b # LESSER THAN EQUAL TO COMPARISION
+print("result of",a,"<=",b,"is :",result)
+
+result = a!=b  # NOT EQUAL COMPARISION
+print("result of",a,"!=",b,"is :",result)
+ 
+result = a==b
+print("result of",a,"==",b,"is :",result)
