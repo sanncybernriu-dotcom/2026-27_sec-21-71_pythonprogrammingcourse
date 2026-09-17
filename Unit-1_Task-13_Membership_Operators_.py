ad9b1bef7c3dcfe729f5f2e52694bdc4c97d26e4
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-17 23:52:43 +0530

    Add files via upload

---
+++ Unit-1_Task-13_Membership_Operators_.py
@@ -0,0 +1,12 @@
+#MEMBERSHIP OPERATOR
+#IN operator
+a=[3,4,5,7]
+b=4
+print("the output of ",b,"in",a,":",b in a)
+print("the output of ",b,"in",a,":",b not in a)
+
+#NOT IN operator
+c=[0,9,8,7,6]
+d=5
+print("the output of ",d,"not in",c,":",d not in c)
+print("the output of ",d,"in",c,":",d in c)
