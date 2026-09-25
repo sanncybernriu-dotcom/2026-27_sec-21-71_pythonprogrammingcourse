f63a963bb45cda8d507bde01168ef34487aef81c
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-25 14:22:23 +0530

    Fix formatting and clean up rectangle area program

--- Write a python program to calculate area and perimeter of a rectangle.py
+++ Write a python program to calculate area and perimeter of a rectangle.py
@@ -2,9 +2,9 @@
 
   # Write a python program to calculate area and perimeter of a rectangle
 
-"""length=-9
+length=-9
 breadth=5
 area=length*breadth
 print("area:", area)
 perimeter=2*(length+breadth)
-print("perimeter:", perimeter)"""
+print("perimeter:", perimeter)
