48b622d6ba6d7f4c67f0db68276a494efc2f0974
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-25 14:23:39 +0530

    Fix formatting and clean up swap program

--- unit-1_task-5_swap_two_numbers.py
+++ unit-1_task-5_swap_two_numbers.py
@@ -2,10 +2,10 @@
 
      # WRITE A PYTHON PROGRAM TO SWAP TWO NUMBERS WITHOUT USING A THIRD VAIRABBLE:
  
-"""a=int(input("Enter a number a: "))
+a=int(input("Enter a number a: "))
 b=int(input("Enter a number b: "))
 a=a+b
 b=a-b   
 a=a-b
 print("After swapping the values : a =", a)
-print("After swapping the values : b =", b)"""
+print("After swapping the values : b =", b)
