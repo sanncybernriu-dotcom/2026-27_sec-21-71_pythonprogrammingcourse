a471d0688efb2539c230b2e5377c45ddbb6ee4af
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-25 14:21:55 +0530

    Fix formatting and remove unnecessary newlines

--- unit-1 task-4_celcius_to_farenheit.py
+++ unit-1 task-4_celcius_to_farenheit.py
@@ -2,7 +2,6 @@
              #python programing 4th experiement::4::
 
      #Write a python program to convert temperature from Celsius to Fahrenheit
-
-"""celsius=float(input("Enter temperature in Celsius: "))
+celsius=float(input("Enter temperature in Celsius: "))
 fahrenheit=(celsius * 9/5)+32
-print("Temperature in Fahrenheit:", fahrenheit)"""
+print("Temperature in Fahrenheit:", fahrenheit)
