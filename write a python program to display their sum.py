8fdf92cceae79e694f7cd561607a1e3db04bd66c
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-25 14:24:21 +0530

    Fix formatting and update sum calculation program

--- write a python program to display their sum.py
+++ write a python program to display their sum.py
@@ -2,8 +2,8 @@
 
 # Write a python program to read two numbers and display their sum
 
-"""num1=int(input("Enter num1"))
+num1=int(input("Enter num1"))
 num2=int(input("Enter num2"))
 result=num1+num2
-print(result)"""
+print(result)
 
