34e45d576f4e047a68dedac0778657e9b03a6665
sanncybernriu-dotcom <sanncybernriu@gmail.com>
2026-09-25 14:19:58 +0530

    Fix formatting and clean up comments in sum program

--- unit-1_task-1_sum_of_two_numbers.py
+++ unit-1_task-1_sum_of_two_numbers.py
@@ -2,7 +2,7 @@
 
 # Write a python program to read two numbers and display their sum
 
-"""num1=int(input("Enter num1"))
+""num1=int(input("Enter num1"))
 num2=int(input("Enter num2"))
 result=num1+num2
 print(result)"""
